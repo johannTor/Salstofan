3f2858447b418f64083919c07483a47260f0885d
Johann <johannth88@gmail.com>
2020-09-08 18:56:22 +0000

    Appointment takka virkni

--- main.js
+++ main.js
@@ -2,11 +2,21 @@
 // If it is hidden, then show it otherwise hide it again.
 function myFunction() {
     // x is the html element with id #myLinks
-      var x = document.getElementById("myLinks");
-      // if the element contains the class hideMobile remove it, otherwise add it
-      if (x.classList.contains("hideMobile")) {
-        x.classList.remove("hideMobile")
-      } else {
-        x.classList.add("hideMobile");
-      }
-  }
+    var x = document.getElementById("myLinks");
+    // if the element contains the class hideMobile remove it, otherwise add it
+    if (x.classList.contains("hideMobile")) {
+      x.classList.remove("hideMobile")
+    } else {
+      x.classList.add("hideMobile");
+    }
+}
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+  document.getElementById("appointmentButton").style.display = "none";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+  document.getElementById("appointmentButton").style.display = "block";
+}
