8f55333f2f5c060c6b042b1db68f3599ab9d0ba6
Johann <johannth88@gmail.com>
2020-09-10 14:08:48 +0000

    Some javascript added, staff section and footer

--- main.js
+++ main.js
@@ -6,14 +6,21 @@ function myFunction() {
     // if the element contains the class hideMobile remove it, otherwise add it
     if (x.classList.contains("hideMobile")) {
       x.classList.remove("hideMobile")
+      // if we are showing the mobile navbar then close the form
+      closeForm()
     } else {
       x.classList.add("hideMobile");
     }
 }
 
 function openForm() {
+  var x = document.getElementById("myLinks");
   document.getElementById("myForm").style.display = "block";
   document.getElementById("appointmentButton").style.display = "none";
+  // If we are opening the form while the mobile menu is visible then hide it
+  if (x.classList.contains("hideMobile") != true) {
+    x.classList.add("hideMobile");
+  }
 }
 
 function closeForm() {
